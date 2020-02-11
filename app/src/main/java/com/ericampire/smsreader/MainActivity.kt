b4edc5d89226ec3d5d92e18636f6382e3242cf0d
Eric Ampire <ericampire.top@gmail.com>
2020-02-11 13:26:54 +0200

    Project Initialization

---
+++ app/src/main/java/com/ericampire/smsreader/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ericampire.smsreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
